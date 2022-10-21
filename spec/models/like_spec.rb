1d5278e62f4071d7d59e2bc3b1fa629c1df9f48f
Tushar <singhtushar348@gmail.com>
2022-10-21 20:32:12 +0530

    Add specs test and Model Validations'

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  describe 'Should increment likes_counter' do 
+  describe 'Should increment likes_counter' do
     author = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-    post = Post.new(author: author, title: 'Tushar', comments_count: 1, likes_count: 2)
+    post = Post.new(author:, title: 'Tushar', comments_count: 1, likes_count: 2)
     before { post.likes_count = 5 }
     it 'Should increment likes_counter to be 6' do
-      like = Like.new(post: post)
+      like = Like.new(post:)
       like.update_likes_count
       expect(post.likes_count).to eq(6)
     end
-  end 
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,12 +12,12 @@ RSpec.describe User, type: :model do
     user = User.new(name: 'Tushar', posts_count: 1)
     expect(user).to be_valid
   end
-  
-  describe 'Should return 3 recent post' do 
+
+  describe 'Should return 3 recent post' do
     user = User.create(name: 'Tushar')
-    before { 4.times { |n| Post.create(author_id: user.id, title: "Post #{n}", text: "Post Decrtiption") } } 
-    it 'User should have 3 recent posts' do 
-      expect(user.recent_posts).to eq(user.posts.last(3)) 
-    end 
-  end 
+    before { 4.times { |n| Post.create(author_id: user.id, title: "Post #{n}", text: 'Post Decrtiption') } }
+    it 'User should have 3 recent posts' do
+      expect(user.recent_posts).to eq(user.posts.last(3))
+    end
+  end
 end
