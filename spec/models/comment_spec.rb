0691dc9a4020c120abd499adfff0f4d5804a0027
Tushar <singhtushar348@gmail.com>
2022-10-21 20:26:42 +0530

    Add test cases for Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Should increment comments_counter' do 
+    author = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    post = Post.new(author: author, title: 'Tushar', comments_count: 1, likes_count: 2)
+    before { post.comments_count = 5 }
+    it 'Should increment comments_counter to be 6' do
+      comment = Comment.new(post: post)
+      comment.update_comments_count
+      expect(post.comments_count).to eq(6)
+    end
+  end 
 end
