1d779c59d0b50f995c312bfd8615b55aec5aecd4
Tushar <singhtushar348@gmail.com>
2022-10-26 23:18:21 +0530

    Get users data from user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
 
   def show
-    puts params
+    @author = User.find(params[:id])
   end
 end
