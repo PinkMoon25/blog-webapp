437154ccde899b15f03af1c44d6c0a46278f6c7b
Tushar <singhtushar348@gmail.com>
2022-10-28 18:22:34 +0530

    Add and modify routes for all appropriate actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root 'home#index'
-  # get "/users", to: 'users#index'
-  # get '/users/:id', to: 'users#show'
-  # get '/users/:user_id/posts', to: 'posts#index'
-  # get '/users/:user_id/posts/:id', to: 'posts#show'
-  resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show]
-  end
+  get "/users", to: 'users#index', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_recent_posts'
+  post '/users/:user_id/posts', to: 'posts#create'
+  get '/users/:user_id/posts/new', to: 'posts#new', as: 'new_post'
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_all_posts'
+  post '/users/:user_id/posts/:id', to: 'comments#create'
+  post '/users/:user_id/posts/:id/likes', to: 'likes#create', as: 'like'
 end
