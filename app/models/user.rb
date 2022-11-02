2d9405f993f9e3ca428bfaa4c0b5435acb131334
Tushar <singhtushar348@gmail.com>
2022-11-02 17:01:19 +0530

    Set default counter for Post and User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
-
+  before_validation :set_counter
   validates :name, presence: true
   validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  def set_counter
+    self.posts_count = 0 if posts_count.nil?
+  end
+  
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
