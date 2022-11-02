937a9c1af00f6a2ce2edf954e9695ffad7d239ce
Tushar <singhtushar348@gmail.com>
2022-11-03 01:08:16 +0530

    fix rubocop issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,9 @@ class User < ApplicationRecord
   validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def set_counter
-    self.posts_count = 0 if self.posts_count.nil?
+    self.posts_count = 0 if posts_count.nil?
   end
-  
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
