cc89f258f39e9a8b0b3d8dcbe871e23d86f7b0ad
Tushar <singhtushar348@gmail.com>
2022-11-04 02:26:31 +0530

    Add authorisation for current user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,9 +1,17 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, :all
+    return unless user.present?
+
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
+    can :create, Comment
+    can :create, Post
+    return unless user.admin?
+
+    can :manage, :all
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
